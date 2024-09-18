78a0ee8a81851740235595ebda5a768b189c9c25
Mohamed Osama Ali <ana20332183@gmail.com>
2024-09-18 21:43:41 +0300

    Add project files.

---
+++ Demo.DAL/Contexts/MvcAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Contexts
+{
+	public class MvcAppDbContext : DbContext
+	{
+        public MvcAppDbContext(DbContextOptions<MvcAppDbContext> options) : base(options)
+        {
+            
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //	=> optionsBuilder.UseSqlServer("server = .; database = MvcAppDb ; Trusted_Connection = true;");
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,43 @@
+﻿using Demo.BLL.Interfaces;
+using Demo.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+	public class DepartmentController : Controller
+	{
+		private readonly IDepartmentRepository _departmentRepository;
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+			_departmentRepository = departmentRepository;
+		}
+        public IActionResult Index()
+		{
+			var departments = _departmentRepository.GetAll();
+			return View(departments);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Department department)
+		{
+			if (ModelState.IsValid) //Server Side Validation
+			{
+				_departmentRepository.Add(department);
+				return RedirectToAction(nameof(Index));
+			}
+			return View(department);
+		}
+		public IActionResult Details(int? id)
+		{
+			if (id is null)
+				return BadRequest();
+			var department = _departmentRepository.GetById(id.Value);
+			if(department is null)
+				return NotFound();
+			return View(department);
+		}
+	}
+}
