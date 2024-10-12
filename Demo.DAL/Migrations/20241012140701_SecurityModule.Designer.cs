c6a0f862c9a4da6aa81be25c85ccbf6be490251a
Mohamed Osama Ali <ana20332183@gmail.com>
2024-10-12 17:50:09 +0300

    Account Controller

--- Demo.DAL/Migrations/20241012140701_SecurityModule.Designer.cs
+++ Demo.DAL/Migrations/20241012140701_SecurityModule.Designer.cs
@@ -4,14 +4,16 @@ using Demo.DAL.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Demo.DAL.Migrations
 {
     [DbContext(typeof(MvcAppDbContext))]
-    partial class MvcAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241012140701_SecurityModule")]
+    partial class SecurityModule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
